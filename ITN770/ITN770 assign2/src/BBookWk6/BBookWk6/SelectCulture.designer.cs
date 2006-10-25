074a8aa5b37d5f05e5a978f427c9d0eb548fd0dc
kelvin.rc <kelvin.rc@309dda2c-1e1c-0410-8d67-b5ebee65c23b>
2006-10-25 13:18:14 +0000

    finish i18n messagebox

--- ITN770/ITN770 assign2/src/BBookWk6/BBookWk6/SelectCulture.designer.cs
+++ ITN770/ITN770 assign2/src/BBookWk6/BBookWk6/SelectCulture.designer.cs
@@ -28,77 +28,77 @@ namespace BirthdayBook
         /// </summary>
         private void InitializeComponent()
         {
-            this.optNativeName = new System.Windows.Forms.RadioButton();
-            this.optEnglishName = new System.Windows.Forms.RadioButton();
-            this.optDisplayName = new System.Windows.Forms.RadioButton();
-            this.chkShowAll = new System.Windows.Forms.CheckBox();
+            this.radNativeName = new System.Windows.Forms.RadioButton();
+            this.radEnglishName = new System.Windows.Forms.RadioButton();
+            this.radDispalyName = new System.Windows.Forms.RadioButton();
+            this.chbShowAllCultures = new System.Windows.Forms.CheckBox();
             this.cmdCancel = new System.Windows.Forms.Button();
             this.cmdOK = new System.Windows.Forms.Button();
             this.cboUICulture = new System.Windows.Forms.ComboBox();
             this.cboCulture = new System.Windows.Forms.ComboBox();
             this.lblUICulture = new System.Windows.Forms.Label();
             this.lblCulture = new System.Windows.Forms.Label();
-            this.Label4 = new System.Windows.Forms.Label();
-            this.Label5 = new System.Windows.Forms.Label();
-            this.Label3 = new System.Windows.Forms.Label();
-            this.Label2 = new System.Windows.Forms.Label();
-            this.Label1 = new System.Windows.Forms.Label();
+            this.lblSelectUICul = new System.Windows.Forms.Label();
+            this.lblSelectCul = new System.Windows.Forms.Label();
+            this.lblInstalledUICul = new System.Windows.Forms.Label();
+            this.lblInstalledCul = new System.Windows.Forms.Label();
+            this.lblIntroduction = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // optNativeName
+            // radNativeName
             // 
-            this.optNativeName.Location = new System.Drawing.Point(192, 274);
-            this.optNativeName.Name = "optNativeName";
-            this.optNativeName.Size = new System.Drawing.Size(104, 24);
-            this.optNativeName.TabIndex = 29;
-            this.optNativeName.Text = "Native Name";
-            this.optNativeName.CheckedChanged += new System.EventHandler(this.optNativeName_CheckedChanged);
+            this.radNativeName.Location = new System.Drawing.Point(197, 281);
+            this.radNativeName.Name = "radNativeName";
+            this.radNativeName.Size = new System.Drawing.Size(156, 24);
+            this.radNativeName.TabIndex = 29;
+            this.radNativeName.Text = "[NATIVE NAME]";
+            this.radNativeName.CheckedChanged += new System.EventHandler(this.optNativeName_CheckedChanged);
             // 
-            // optEnglishName
+            // radEnglishName
             // 
-            this.optEnglishName.Location = new System.Drawing.Point(192, 250);
-            this.optEnglishName.Name = "optEnglishName";
-            this.optEnglishName.Size = new System.Drawing.Size(104, 24);
-            this.optEnglishName.TabIndex = 28;
-            this.optEnglishName.Text = "English Name";
-            this.optEnglishName.CheckedChanged += new System.EventHandler(this.optEnglishName_CheckedChanged);
+            this.radEnglishName.Location = new System.Drawing.Point(197, 256);
+            this.radEnglishName.Name = "radEnglishName";
+            this.radEnglishName.Size = new System.Drawing.Size(156, 24);
+            this.radEnglishName.TabIndex = 28;
+            this.radEnglishName.Text = "[ENGLISH NAME]";
+            this.radEnglishName.CheckedChanged += new System.EventHandler(this.optEnglishName_CheckedChanged);
             // 
-            // optDisplayName
+            // radDispalyName
             // 
-            this.optDisplayName.Checked = true;
-            this.optDisplayName.Location = new System.Drawing.Point(192, 226);
-            this.optDisplayName.Name = "optDisplayName";
-            this.optDisplayName.Size = new System.Drawing.Size(104, 24);
-            this.optDisplayName.TabIndex = 27;
-            this.optDisplayName.TabStop = true;
-            this.optDisplayName.Text = "Display Name";
-            this.optDisplayName.CheckedChanged += new System.EventHandler(this.optDisplayName_CheckedChanged);
+            this.radDispalyName.Checked = true;
+            this.radDispalyName.Location = new System.Drawing.Point(197, 226);
+            this.radDispalyName.Name = "radDispalyName";
+            this.radDispalyName.Size = new System.Drawing.Size(156, 24);
+            this.radDispalyName.TabIndex = 27;
+            this.radDispalyName.TabStop = true;
+            this.radDispalyName.Text = "[DISPLAY NAME]";
+            this.radDispalyName.CheckedChanged += new System.EventHandler(this.optDisplayName_CheckedChanged);
             // 
-            // chkShowAll
+            // chbShowAllCultures
             // 
-            this.chkShowAll.Location = new System.Drawing.Point(16, 226);
-            this.chkShowAll.Name = "chkShowAll";
-            this.chkShowAll.Size = new System.Drawing.Size(152, 24);
-            this.chkShowAll.TabIndex = 26;
-            this.chkShowAll.Text = "Show All Cultures";
-            this.chkShowAll.CheckedChanged += new System.EventHandler(this.chkShowAll_CheckedChanged);
+            this.chbShowAllCultures.Location = new System.Drawing.Point(16, 226);
+            this.chbShowAllCultures.Name = "chbShowAllCultures";
+            this.chbShowAllCultures.Size = new System.Drawing.Size(175, 24);
+            this.chbShowAllCultures.TabIndex = 26;
+            this.chbShowAllCultures.Text = "[SHOW ALL CULTURES]";
+            this.chbShowAllCultures.CheckedChanged += new System.EventHandler(this.chkShowAll_CheckedChanged);
             // 
             // cmdCancel
             // 
-            this.cmdCancel.Location = new System.Drawing.Point(336, 258);
+            this.cmdCancel.Location = new System.Drawing.Point(384, 257);
             this.cmdCancel.Name = "cmdCancel";
             this.cmdCancel.Size = new System.Drawing.Size(136, 23);
             this.cmdCancel.TabIndex = 25;
-            this.cmdCancel.Text = "Cancel";
+            this.cmdCancel.Text = "[CANCEL]";
             this.cmdCancel.Click += new System.EventHandler(this.cmdExit_Click);
             // 
             // cmdOK
             // 
-            this.cmdOK.Location = new System.Drawing.Point(336, 226);
+            this.cmdOK.Location = new System.Drawing.Point(384, 226);
             this.cmdOK.Name = "cmdOK";
             this.cmdOK.Size = new System.Drawing.Size(136, 23);
             this.cmdOK.TabIndex = 24;
-            this.cmdOK.Text = "OK";
+            this.cmdOK.Text = "[OK]";
             this.cmdOK.Click += new System.EventHandler(this.cmdStart_Click);
             // 
             // cboUICulture
@@ -145,70 +145,70 @@ namespace BirthdayBook
             this.lblCulture.Size = new System.Drawing.Size(240, 23);
             this.lblCulture.TabIndex = 16;
             // 
-            // Label4
+            // lblSelectUICul
             // 
-            this.Label4.Location = new System.Drawing.Point(280, 170);
-            this.Label4.Name = "Label4";
-            this.Label4.Size = new System.Drawing.Size(240, 21);
-            this.Label4.TabIndex = 23;
-            this.Label4.Text = "Select UI Culture";
+            this.lblSelectUICul.Location = new System.Drawing.Point(280, 170);
+            this.lblSelectUICul.Name = "lblSelectUICul";
+            this.lblSelectUICul.Size = new System.Drawing.Size(240, 21);
+            this.lblSelectUICul.TabIndex = 23;
+            this.lblSelectUICul.Text = "[SELECT UI CULTURE]";
             // 
-            // Label5
+            // lblSelectCul
             // 
-            this.Label5.Location = new System.Drawing.Point(16, 170);
-            this.Label5.Name = "Label5";
-            this.Label5.Size = new System.Drawing.Size(240, 23);
-            this.Label5.TabIndex = 22;
-            this.Label5.Text = "Select Culture";
+            this.lblSelectCul.Location = new System.Drawing.Point(16, 170);
+            this.lblSelectCul.Name = "lblSelectCul";
+            this.lblSelectCul.Size = new System.Drawing.Size(240, 23);
+            this.lblSelectCul.TabIndex = 22;
+            this.lblSelectCul.Text = "[SELECT CULTURE]";
             // 
-            // Label3
+            // lblInstalledUICul
             // 
-            this.Label3.Location = new System.Drawing.Point(280, 122);
-            this.Label3.Name = "Label3";
-            this.Label3.Size = new System.Drawing.Size(240, 21);
-            this.Label3.TabIndex = 21;
-            this.Label3.Text = "Installed UI Culture";
+            this.lblInstalledUICul.Location = new System.Drawing.Point(280, 122);
+            this.lblInstalledUICul.Name = "lblInstalledUICul";
+            this.lblInstalledUICul.Size = new System.Drawing.Size(240, 21);
+            this.lblInstalledUICul.TabIndex = 21;
+            this.lblInstalledUICul.Text = "[INSTALLED UI CULTURE]";
             // 
-            // Label2
+            // lblInstalledCul
             // 
-            this.Label2.Location = new System.Drawing.Point(16, 122);
-            this.Label2.Name = "Label2";
-            this.Label2.Size = new System.Drawing.Size(240, 23);
-            this.Label2.TabIndex = 20;
-            this.Label2.Text = "Installed Culture";
+            this.lblInstalledCul.Location = new System.Drawing.Point(16, 122);
+            this.lblInstalledCul.Name = "lblInstalledCul";
+            this.lblInstalledCul.Size = new System.Drawing.Size(240, 23);
+            this.lblInstalledCul.TabIndex = 20;
+            this.lblInstalledCul.Text = "[INSTALLED CULTURE]";
             // 
-            // Label1
+            // lblIntroduction
             // 
-            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label1.Location = new System.Drawing.Point(24, 18);
-            this.Label1.Name = "Label1";
-            this.Label1.Size = new System.Drawing.Size(496, 104);
-            this.Label1.TabIndex = 15;
-            this.Label1.Text = "This application startup form allows you to reset the Culture and User Interface " +
-                "Culture before starting an application. Use Exit to terminate immediately.";
+            this.lblIntroduction.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIntroduction.Location = new System.Drawing.Point(24, 18);
+            this.lblIntroduction.Name = "lblIntroduction";
+            this.lblIntroduction.Size = new System.Drawing.Size(496, 104);
+            this.lblIntroduction.TabIndex = 15;
+            this.lblIntroduction.Text = "[THIS APPLICATION STARTUP FORM ALLOWS YOU TO RESET THE CULTURE AND USER INTERFACE" +
+                " CULTURE BEFORE STARTING AN APPLICATION. USE EXIT TO TERMINATE IMMEDIATELY]";
             // 
             // SelectCulture
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(536, 317);
-            this.Controls.Add(this.optNativeName);
-            this.Controls.Add(this.optEnglishName);
-            this.Controls.Add(this.optDisplayName);
-            this.Controls.Add(this.chkShowAll);
+            this.Controls.Add(this.radNativeName);
+            this.Controls.Add(this.radEnglishName);
+            this.Controls.Add(this.radDispalyName);
+            this.Controls.Add(this.chbShowAllCultures);
             this.Controls.Add(this.cmdCancel);
             this.Controls.Add(this.cmdOK);
             this.Controls.Add(this.cboUICulture);
             this.Controls.Add(this.cboCulture);
             this.Controls.Add(this.lblUICulture);
             this.Controls.Add(this.lblCulture);
-            this.Controls.Add(this.Label4);
-            this.Controls.Add(this.Label5);
-            this.Controls.Add(this.Label3);
-            this.Controls.Add(this.Label2);
-            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.lblSelectUICul);
+            this.Controls.Add(this.lblSelectCul);
+            this.Controls.Add(this.lblInstalledUICul);
+            this.Controls.Add(this.lblInstalledCul);
+            this.Controls.Add(this.lblIntroduction);
             this.Name = "SelectCulture";
-            this.Text = "SelectCulture";
+            this.Text = "[SELECT CULTURE]";
             this.Load += new System.EventHandler(this.SelectCulture_Load);
             this.ResumeLayout(false);
 
@@ -216,21 +216,21 @@ namespace BirthdayBook
 
         #endregion
 
-        internal System.Windows.Forms.RadioButton optNativeName;
-        internal System.Windows.Forms.RadioButton optEnglishName;
-        internal System.Windows.Forms.RadioButton optDisplayName;
-        internal System.Windows.Forms.CheckBox chkShowAll;
+        internal System.Windows.Forms.RadioButton radNativeName;
+        internal System.Windows.Forms.RadioButton radEnglishName;
+        internal System.Windows.Forms.RadioButton radDispalyName;
+        internal System.Windows.Forms.CheckBox chbShowAllCultures;
         internal System.Windows.Forms.Button cmdCancel;
         internal System.Windows.Forms.Button cmdOK;
         internal System.Windows.Forms.ComboBox cboUICulture;
         internal System.Windows.Forms.ComboBox cboCulture;
         internal System.Windows.Forms.Label lblUICulture;
         internal System.Windows.Forms.Label lblCulture;
-        internal System.Windows.Forms.Label Label4;
-        internal System.Windows.Forms.Label Label5;
-        internal System.Windows.Forms.Label Label3;
-        internal System.Windows.Forms.Label Label2;
-        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Label lblSelectUICul;
+        internal System.Windows.Forms.Label lblSelectCul;
+        internal System.Windows.Forms.Label lblInstalledUICul;
+        internal System.Windows.Forms.Label lblInstalledCul;
+        internal System.Windows.Forms.Label lblIntroduction;
 
     }
 }
