39ad3d2b25be1669180d772636655d7f638a7fe8
kelvin.rc <kelvin.rc@309dda2c-1e1c-0410-8d67-b5ebee65c23b>
2007-03-04 14:01:09 +0000

    added in state and stroke class

---
+++ Dissertition/sourcecode/hmmhandwriting/stroke.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "state.h"
+using namespace std;
+
+class Stroke{
+public:
+	State state[3];
+	
+	Stroke();
+}
+
+Stroke::Stroke(){
+	for(int i=0; i<16; i++){
+		state[i] = NULL;
+	}
+}
